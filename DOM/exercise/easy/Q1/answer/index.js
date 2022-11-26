e9ed52f6209c50d8d09d8616a3d7803d8f3608a3
XinXiaoIsMe <ccattempt@163.com>
2022-11-26 20:41:35 +0800

    fix: 修改代码逻辑

--- DOM/exercise/easy/Q1/answer/index.js
+++ DOM/exercise/easy/Q1/answer/index.js
@@ -2,14 +2,14 @@ export default function (nodeA, nodeB) {
   const mapA = new Map() // 缓存nodeA的DOM链
   const mapB = new Map() // 缓存nodeB的DOM链
 
-  while (nodeA && nodeB) {
+  while (nodeA || nodeB) {
     if (mapA.has(nodeB)) return nodeB // 查看nodeB是否在nodeA的DOM链上
     if (mapB.has(nodeA)) return nodeA // 查看nodeA是否在nodeB的DOM链上
 
     mapA.set(nodeA, 'a')
     mapB.set(nodeB, 'b')
 
-    nodeA = nodeA.parentNode
-    nodeB = nodeB.parentNode
+    nodeA && (nodeA = nodeA.parentNode)
+    nodeB && (nodeB = nodeB.parentNode)
   }
 }

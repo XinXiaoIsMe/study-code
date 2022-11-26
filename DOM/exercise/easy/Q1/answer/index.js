420891a4b4a7de8abd2c75f52af8e147b9e51c9a
XinXiaoIsMe <ccattempt@163.com>
2022-11-26 20:37:08 +0800

    chore: 添加注释

--- DOM/exercise/easy/Q1/answer/index.js
+++ DOM/exercise/easy/Q1/answer/index.js
@@ -1,10 +1,10 @@
 export default function (nodeA, nodeB) {
-  const mapA = new Map()
-  const mapB = new Map()
+  const mapA = new Map() // 缓存nodeA的DOM链
+  const mapB = new Map() // 缓存nodeB的DOM链
 
   while (nodeA && nodeB) {
-    if (mapA.has(nodeB)) return nodeB
-    if (mapB.has(nodeA)) return nodeA
+    if (mapA.has(nodeB)) return nodeB // 查看nodeB是否在nodeA的DOM链上
+    if (mapB.has(nodeA)) return nodeA // 查看nodeA是否在nodeB的DOM链上
 
     mapA.set(nodeA, 'a')
     mapB.set(nodeB, 'b')
